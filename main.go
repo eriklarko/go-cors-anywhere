3ed4606d383f1e610df7498cd39a04d85b82fc23
Erik Larkö <erik.larko@wattpad.com>
2020-11-15 14:00:00 +0100

    Read URL to forward to from request

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"strings"
 )
 
 func main() {
@@ -24,7 +25,7 @@ func main() {
 }
 
 func httpHandler(res http.ResponseWriter, req *http.Request) {
-	urlToForwardTo, err := url.Parse("https://icanhazip.com")
+	urlToForwardTo, err := getURLToForwardTo(req)
 	if err != nil {
 		fmt.Fprintf(res, "unable to parse URL: %v", err)
 		return
@@ -32,4 +33,21 @@ func httpHandler(res http.ResponseWriter, req *http.Request) {
 
 	httputil.NewSingleHostReverseProxy(urlToForwardTo).
 		ServeHTTP(res, req)
+}
+
+func getURLToForwardTo(req *http.Request) (*url.URL, error) {
+	// We expect request in the format
+	//   protocol://host:port/url-to-foward to
+	// where url-to-foward to can be (non-exhaustive)
+	//   * google.com
+	//   * https://google.com
+	//   * google.com:443
+	//
+	// If the parameter `req` is a request to http://example.com/hello
+	// then `req.URL.Path` is the string "/hello", so we simply
+	// take the `req.URL.Path` without the first slash to be the URL
+	// to forward to.
+
+	rawURL := strings.TrimPrefix(req.URL.Path, "/")
+	return url.Parse(rawURL)
 }
---
+++ main_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetURLToForwardTo(t *testing.T) {
+	testCases := map[string]struct {
+		// the URL we want to forward to
+		url string
+
+		// the result we expect from the getURLToForwardTo function
+		expected string
+	}{
+		"only host name": {
+			url:      "foo.com",
+			expected: "foo.com",
+		},
+		"with protocol and port": {
+			url:      "http://foo.com:1337",
+			expected: "http://foo.com:1337",
+		},
+		"with protocol": {
+			url:      "http://foo.com",
+			expected: "http://foo.com",
+		},
+		"with port": {
+			url:      "foo.com:1337",
+			expected: "foo.com:1337",
+		},
+	}
+
+	serverURL := "https://example.com:8080"
+	for name, testCase := range testCases {
+
+		t.Run(name, func(t *testing.T) {
+			req, err := http.NewRequest(
+				"GET", fmt.Sprintf("%s/%s", serverURL, testCase.url),
+				nil,
+			)
+			assert.NoError(t, err, "failed creating test request")
+
+			actual, err := getURLToForwardTo(req)
+			assert.NoError(t, err, "unable to read URL to forward to from test request")
+
+			assert.Equal(t, testCase.expected, actual.String())
+		})
+	}
+}
